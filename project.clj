7709393825406a1d7e49808ef26bde97f2f36787
Joshua Miller <elefantstn@gmail.com>
2013-11-01 12:20:29 -0400

    Set project group for Clojars deploy.
    (Fixes #1)

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject io.joshuamiller/exif-processor "0.1.1"
+(defproject io.joshmiller/exif-processor "0.1.1"
   :description "A lightweight Clojure wrapper around the exif processing facility in Drew Oakes' metadata-extractor"
   :url "http://github.com/joshuamiller/exif-processor"
-  :min-lein-version "0.2.0"
+  :min-lein-version "2.1.0"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.4.0"]
